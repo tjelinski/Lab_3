02523bb8e99ddb0b3159db0af400289eb882e7f1
tjelinski <mail@tomaszjelinski.com>
2023-11-25 21:10:09 +0100

    part one from 1 to 7

--- src/Main.java
+++ src/Main.java
@@ -67,6 +67,17 @@ public class Main {
         System.out.println(Arrays.toString(tab));
         reverseTab(tab);
 
+        //8. wykonanie kodu z instrukcji laboratoryjnej
+        // args to parametry programu przekazane z wiersza poleceń
+        // args.length to liczba przekazanych parametrów
+        System.out.println("Liczba parametrów: " + args.length);
+        // Wyświetlenie wszystkich przekazanych parametrów
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Parametr " + i + ": " + args[i]);
+        }
+
+        //9.
+
     }
 
     private static void reverseTab(int[] tab) {
